a212a87823555768bfb8ccaef0fcebf79d6baf13
felix <sparkyxu@hotmail.com>
2018-11-01 11:32:30 -0700

    rec and line

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Game from './partials/Game'
 const game = new Game('game', 512, 256);
 
 (function gameLoop() {
+
     game.render();
     requestAnimationFrame(gameLoop);
 })();
--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,15 +1,33 @@
+//import SVG_NS from setting.js
+import {
+	SVG_NS
+} from "../settings";
+import Board from "./Board";
+
 export default class Game {
 
 	constructor(element, width, height) {
 		this.element = element;
 		this.width = width;
 		this.height = height;
-	
+
 		// Other code goes here...
+		this.gameElement = document.getElementById(this.element);
+		this.board = new Board(this.width, this.height);
 	}
 
+	// More code goes here... render=drew
 	render() {
-		// More code goes here...
+		this.gameElement.innerHTML = ""; //fix bug on index.js gameloop eg.run console.log 
+		let svg = document.createElementNS(SVG_NS, "svg");
+		svg.setAttributeNS(null, 'width', this.width);
+		svg.setAttributeNS(null, 'height', this.height);
+		svg.setAttributeNS(null, 'viewBox', `0 0 ${this.width} ${this.height}`);
+		this.gameElement.appendChild(svg);
+		this.board.render(svg)
+
+
+
 	}
 
 }
