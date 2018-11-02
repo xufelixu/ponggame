692cea74950376fa1b05a720c40f9721723ea771
felix <sparkyxu@hotmail.com>
2018-11-02 09:44:38 -0700

    ball direction

--- src/partials/Ball.js
+++ src/partials/Ball.js
@@ -16,16 +16,28 @@ export default class Ball {
     reset() {
         this.x = this.boardWidth / 2;
         this.y = this.boardHeight / 2;
+        this.vy = 0;
+        while (this.vy === 0) {
+            this.vy = Math.floor(Math.random() * 10 - 5);
+
+        }
+
+        this.vx = this.direction * (6 - Math.abs(this.vy)); //this.direction is to reset the direction 
+
     }
 
     //ball reset 
 
     render(svg) {
+
+        this.x += this.vx;
+        this.y += this.vy;
+
         //drew ball
         let circle = document.createElementNS(SVG_NS, "circle");
         circle.setAttributeNS(null, 'r', this.radius);
-        circle.setAttributeNS(null, 'cx', this.x / 2); // x of the centre point
-        circle.setAttributeNS(null, 'cy', this.y / 2); // y of the centre point
+        circle.setAttributeNS(null, 'cx', this.x); // x of the centre point
+        circle.setAttributeNS(null, 'cy', this.y); // y of the centre point
         circle.setAttributeNS(null, 'fill', "white");
         svg.appendChild(circle);
 
